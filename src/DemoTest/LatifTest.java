a023fa5c576df0702ec83824f0a10d5971b5d8bb
khsed <khsed@10.0.0.89>
2023-11-17 10:41:38 -0500

    practice moving project to remote repository

---
+++ src/DemoTest/LatifTest.java
@@ -0,0 +1,18 @@
+package DemoTest;
+
+public class LatifTest {
+	
+	public static String name;
+	static int age;
+	
+	public static void main(String[] args) {
+		
+		name = "Latif";
+		age = 34;
+		
+		System.out.println(name );
+		
+		
+	}
+
+}
